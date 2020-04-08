68aced52203d36b24e90cf33a8500f9ac2a23246
zq99299 <99299684@qq.com>
2020-04-08 17:50:25 +0800

    配置优化

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -65,7 +65,7 @@ module.exports = {
             serviceWorker: true,
             updatePopup: {
                 message: "有新内容更新啦~",
-                buttonText: "立即获取新内容（点击后会自动刷新，变动较大可能会比较明显看到页面变化）"
+                buttonText: "立即获取新内容，确定后稍后自动刷新"
             }
         }],
         ['@vuepress/medium-zoom', {
